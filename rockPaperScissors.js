e329a111c0bb4b41e8c5e90635655bdae7df9152
Temery1213 <temery1213@gmail.com>
2023-08-02 13:31:15 -0500

    computer choice randomized complete

---
+++ rockPaperScissors.js
@@ -0,0 +1,16 @@
+// get computer choice //
+
+function getComputerChoice () {
+let options = ["rock", "paper", "scissors"];
+let choice = options[Math.floor(Math.random()*options.length)];
+return choice
+}
+const computerChoice = getComputerChoice();
+console.log(computerChoice)
+// get player choice // 
+
+// count rounds //
+
+// sum up the score //
+
+// declare a winner // 
